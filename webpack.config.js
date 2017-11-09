3af71e68802d6a1809562a7ca21b240157226d4a
Taly <vitalik7tv@yandex.ru>
2017-11-09 08:55:36 +0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './public/src/entry.js',
+    output: {
+        filename: './public/bundle/bundle.js'
+    },
+    module: {
+        rules: [
+            /**
+             * Process CSS
+             */
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract([
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            minimize: 1,
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ])
+            },
+
+            /**
+             * Process JS files
+             */
+            {
+              test : /\.js$/,
+              use: [
+                {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: [ 'env' ],
+                    }
+                },
+                {
+                    loader: 'eslint-loader',
+                    options: {
+                        fix: true
+                    }
+                }
+              ]
+            }
+        ]
+    },
+    plugins: [
+        /** Build separated styles bundle */
+        new ExtractTextPlugin({
+            filename: './public/bundle/bundle.css',
+            allChunks: true,
+        }),
+        /** Minify JS and CSS */
+        new webpack.optimize.UglifyJsPlugin({
+          sourceMap: true
+        }),
+    ],
+    watch: true,
+    devtool: 'source-map'
+};
