15b3bcfe12e6a0d6b15ac45feee3e4004b2f479c
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-12 17:19:43 +0300

    Da budet tak

--- views/components/header.php
+++ views/components/header.php
@@ -1,32 +1,32 @@
 <div class="header__wrapper">
-<header class="header">
-    <div class="header__itmo"></div>
+    <header class="header">
+        <div class="header__itmo"></div>
 
-    <nav class="navigation--mobile">
-        <i class="navigation__menu-opener js-navigation__menu-opener fa fa-bars"></i>
+        <nav class="navigation--mobile">
+            <i class="navigation__menu-opener js-navigation__menu-opener fa fa-bars"></i>
+            <div class="header__logo"></div>
+        </nav>
+
         <div class="header__logo"></div>
-    </nav>
 
-    <div class="header__logo"></div>
+        <nav class="navigation">
 
-    <nav class="navigation">
+            <a href="#idea" class='navigation__item'><div>Идея</div></a>
+            <a href="#heads" class='navigation__item'><div>Ведущие</div></a>
+            <a href="#guests" class='navigation__item'><div>Гости</div></a>
+            <a href="#place" class='navigation__item'><div>Где и когда?</div></a>
+            <a href="#registration" class='navigation__item'><div>Регистрация</div></a>
+            <a href="#contacts" class='navigation__item'><div>Контакты</div></a>
+            <a href="#organizers" class='navigation__item'><div>Огранизатор</div></a>
 
-        <a href="#idea" class='navigation__item'><div>Идея</div></a>
-        <a href="#heads" class='navigation__item'><div>Ведущие</div></a>
-        <a href="#guests" class='navigation__item'><div>Гости</div></a>
-        <a href="#place" class='navigation__item'><div>Где и когда?</div></a>
-        <a href="#registration" class='navigation__item'><div>Регистрация</div></a>
-        <a href="#contacts" class='navigation__item'><div>Контакты</div></a>
-        <a href="#organizers" class='navigation__item'><div>Огранизатор</div></a>
+        </nav>
 
-    </nav>
+        <div class="header__register-button">Зарегистрироваться</div>
 
-    <div class="header__register-button">Зарегистрироваться</div>
+        <div class="header__text">
+            В случае осуществления внешних проектов чаще всего это желание разрешить проблему, являющуюся причиной недостаточной  в производительности предприятия.
+        </div>
 
-    <div class="header__text">
-        В случае осуществления внешних проектов чаще всего это желание разрешить проблему, являющуюся причиной недостаточной  в производительности предприятия.
-    </div>
-
-</header>
-<div class="header__science"></div>
+    </header>
+    <div class="header__science"></div>
 </div>
