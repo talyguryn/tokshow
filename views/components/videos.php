fafba1963d4166aa4cc3d28d909a049d69649766
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-28 21:07:49 +0300

    Po krasote

--- views/components/videos.php
+++ views/components/videos.php
@@ -3,7 +3,10 @@
 	<div class="videos__arrow-right videos__arrow--desktop"></div>
 	<div class="videos__contents">
 		<div class="videos__column-left">
-			<div class="videos__label">Выпуски</div>
+			<div class="videos__label">Выпуски
+				<div class="videos__arrow-left videos__arrow--mobile"></div>
+				<div class="videos__arrow-right videos__arrow--mobile"></div>
+			</div>
 			<h3 class="videos__title">Выпуск #1</h3>
 			<div class="videos__desc">Описание выпуска</div>
 			<div class="videos__people">выступающие</div>
@@ -11,8 +14,6 @@
 в жизнь за счет проектов</div>
 		</div>
 		<div class="videos__column-right">
-			<div class="videos__arrow-left videos__arrow--mobile"></div>
-			<div class="videos__arrow-right videos__arrow--mobile"></div>
 			<iframe width="640" height="362" src="https://www.youtube.com/embed/JOoz9HB5sSU" frameborder="0" gesture="media" allowfullscreen></iframe>
 		</div>
 	</div>
