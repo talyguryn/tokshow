20cb1c050bd9a70d32ad648f3a0aabc92041974b
Taly <vitalik7tv@yandex.ru>
2017-12-02 02:18:36 +0300

    +

--- views/main.php
+++ views/main.php
@@ -5,7 +5,7 @@
         <title>ТОК шоу – первое late night show, заряженное на сайнс</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1.0,user-scalable=no" />
-        <link rel="stylesheet" href="public/build/bundle.css">
+        <link rel="stylesheet" href="public/build/bundle.css?v=<?= filemtime('public/build/bundle.css') ?>">
     </head>
     <body>
 
@@ -32,6 +32,6 @@
 
         </div>
 
-        <script src="public/build/bundle.js" async></script>
+        <script src="public/build/bundle.js?v=<?= filemtime('public/build/bundle.js') ?>" async></script>
     </body>
 </html>
