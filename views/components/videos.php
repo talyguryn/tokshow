12cd508e6404dd0ee485f4c99aefc4236f0c6e1a
Polina Shneider <beatrixlinkoln@gmail.com>
2017-12-02 10:01:27 +0300

    Video hack

--- views/components/videos.php
+++ views/components/videos.php
@@ -13,8 +13,13 @@
 			<div class="videos__text"></div>
 		</div>
 		<div class="videos__column-right">
-			<iframe width="640" height="362" src="https://www.youtube.com/embed/JOoz9HB5sSU" frameborder="0" gesture="media" allowfullscreen></iframe>
+			<div class="iframe-wrapper">
+			    <div class="iframe-inner">
+			        <!-- a transparent image to keep ratio -->
+			        <img class="iframe-ratio" src="/public/src/img/transparent.png">
+			        <iframe width="640" height="362" src="https://www.youtube.com/embed/JOoz9HB5sSU" frameborder="0" gesture="media" allowfullscreen></iframe>
+				</div>
+			</div>
 		</div>
-	</div>
 	<img class="videos__splash" src="/public/src/img/splash.png">
 </div>
